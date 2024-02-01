461421001409e3dc45831b87338cc951c20964e1
Deyan <drusev14@gmail.com>
2024-02-01 22:28:42 +0200

    Format code
    Signed-off-by: Deyan <drusev14@gmail.com>

--- Car.py
+++ Car.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 28 13:34:32 2024
 
-@author: didko
-"""
+import os
+import sys
 import csv
 import datetime
-import aspose.pdf as ap
 from datetime import date
 import base64
 import maskpass
-import os
+
 class Vehicle:
     days_reserve = 0
 
@@ -22,52 +19,69 @@ class Vehicle:
         self.year = year
         self.color = color
         self.mileage = mileage
+
     def setMileage(self, mileage):
         self.mileage = mileage
+
     def VehicleHistory(self):
         filename = f"vehicle_{self.reg_plate}_info.csv"
         with open(filename, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Brand','Model','Reg_number'])
-            writer.writerow([self.brand,self.model,self.reg_plate])
+            writer.writerow(['Brand', 'Model', 'Reg_number'])
+            writer.writerow([self.brand, self.model, self.reg_plate])
 
-    
 class Car(Vehicle):
     price_per_day = 70
     dbName = "CarsList.csv"
     reserved = False
+
     def __init__(self, brand, model, reg_plate, year, color, mileage, car_type):
-        super().__init__( brand, model, reg_plate, year, color, mileage)
+        super().__init__(brand, model, reg_plate, year, color, mileage)
         self.car_type = car_type
+
     def reserve(self, start_date, days_reserve):
         reserved = True
-        print("Vehicle " + self.brand + " " + self.model + " " + self.reg_plate + " " + " is reserved for " + str(days_reserve) + ". Price is " + str(self.price_per_day*days_reserve))
+        print("Vehicle " + self.brand + " " + self.model + " " + self.reg_plate + " " +
+              " is reserved for " + str(days_reserve) + " days. Price is " + str(self.price_per_day*days_reserve))
+
     def getRentalFee(self, days_reserved):
-         return self.price_per_day*days_reserved
+        return self.price_per_day*days_reserved
+
     def addToDb(self):
-        line = [self.brand, self.model, self.reg_plate, self.year, self.color,self.mileage, self.car_type]
+        line = [self.brand, self.model, self.reg_plate,
+                self.year, self.color, self.mileage, self.car_type]
         with open(self.dbName, mode='a', newline='') as db:
             writer = csv.writer(db)
             writer.writerow(line)
-    
+        print("saved in " + self.dbName)
+
+
 class MotorBike(Vehicle):
     price_per_day = 90
     dbName = "MotorbikesList.csv"
+
     def __init__(self, brand, model, reg_plate, year, color, mileage, cc):
-        super().__init__(brand,model, reg_plate, year, color, mileage)
+        super().__init__(brand, model, reg_plate, year, color, mileage)
         self.cc = cc
+
     def reserve(self, start_date, days_reserve):
         reserved = True
-        print("Vehicle " + self.brand + " " + self.model + " " + self.reg_plate + " is reserved for " + str(days_reserve) + ". Price is " + str(self.price_per_day*days_reserve))
+        print("Vehicle " + self.brand + " " + self.model + " " + self.reg_plate + " is reserved for " +
+              str(days_reserve) + " days. Price is " + str(self.price_per_day*days_reserve))
+
     def getRentalFee(self, days_reserve):
         return self.price_per_day*days_reserve
+
     def addToDb(self):
-        record = [self.brand, self.model, self.reg_plate, self.year, self.color, self.mileage, self.cc]
+        record = [self.brand, self.model, self.reg_plate,
+                  self.year, self.color, self.mileage, self.cc]
         with open(self.dbName, mode='a', newline='') as db:
             writer = csv.writer(db)
             writer.writerow(record)
+        print("saved in " + self.dbName)
+
 class RentalPerson:
-    def __init__(self,firstName, family, nationality, identification_number, card_id, email, phone_number, rent_period):
+    def __init__(self, firstName, family, nationality, identification_number, card_id, email, phone_number, rent_period):
         self.firstName = firstName
         self.family = family
         self.nationality = nationality
@@ -76,14 +90,16 @@ class RentalPerson:
         self.email = email
         self.phone_number = phone_number
         self.rent_period = rent_period
+
     def toString(self):
-        print("Renter " + self.firstName + " " + self.family +" from " + self.nationality + 
+        print("Renter " + self.firstName + " " + self.family + " from " + self.nationality +
               " with id: " + self.identification_number +
-              ", cardId: " + self.card_id + " and contacts: " + self.phone_number + " " + self.email) 
+              ", cardId: " + self.card_id + " and contacts: " + self.phone_number + " " + self.email)
 
 class Document:
     def __init__(self, vehicle, renter):
-        self.name = "Agreement" + vehicle.brand + "-" + vehicle.model + "-" + renter.firstName + "-" + renter.family + ".txt"
+        self.name = "Agreement-" + vehicle.brand + "-" + vehicle.model + \
+            "-" + renter.firstName + "-" + renter.family + ".txt"
         self.renter_name = renter.firstName
         self.renter_family = renter.family
         self.renter_id = renter.identification_number
@@ -94,28 +110,31 @@ class Document:
         self.vehicle_year = vehicle.year
         self.vehicle_color = vehicle.color
         self.vehicle_fee = vehicle.getRentalFee(renter.rent_period)
-        
+
     def generateAgreement(self):
         f = open(self.name, "w")
         f.write("Car Rental Agreement\n")
-        f.write("Agreement between Rent a car company and " + self.renter_name + " " + self.renter_family + "\n")
-        f.write("entered on date " + str(date.today()) +"\n")
-        f.write("1. Rental company agrees to rent to the Renter the following vehicle:\n")
-        f.write("   Brand/model" + self.vehicle_brand + " " + self.vehicle_model + " \n   License plate: " + self.vehicle_plate + "\n Year of manufacture: " + self.vehicle_year + "\n Color: " + self.vehicle_color+"\n")
+        f.write("Agreement between Rent a car company and " +
+                self.renter_name + " " + self.renter_family + "\n")
+        f.write("entered on date " + str(date.today()) + "\n")
+        f.write(
+            "1. Rental company agrees to rent to the Renter the following vehicle:\n")
+        f.write("   Brand/model" + self.vehicle_brand + " " + self.vehicle_model + " \n   License plate: " +
+                self.vehicle_plate + "\n Year of manufacture: " + self.vehicle_year + "\n Color: " + self.vehicle_color+"\n")
         f.write("2. Rental Fees and Payment:")
-        f.write("The Renter agrees to pay the Rental Company the total sum of " + str(self.vehicle_fee) + " leva for the entire rental period.\n")
+        f.write("The Renter agrees to pay the Rental Company the total sum of " +
+                str(self.vehicle_fee) + " leva for the entire rental period.\n")
         f.close()
         print("Agreement generated!")
-        
-   
-    
+
+############################# Main ############################
 def adminAction():
     print("1: Add car ")
     print("2: Add motorbike ")
     print("0 Exit ")
     action = int(input("Enter action "))
     if action == 1:
-        #add new car to cars list
+        # add new car to cars list
         print("Provide car info:")
         brand = input("brand: ")
         model = input("model: ")
@@ -124,11 +143,11 @@ def adminAction():
         color = input("color: ")
         mileage = input("mileage: ")
         car_type = input("type: ")
-        car = Car(brand, model,reg_plate,year,color,mileage,car_type)
+        car = Car(brand, model, reg_plate, year, color, mileage, car_type)
         car.addToDb()
         adminAction()
     elif action == 2:
-        #add new motrobike to bikes
+        # add new motrobike to bikes
         brand = input("brand: ")
         model = input("model: ")
         reg_plate = input("license plate: ")
@@ -143,11 +162,11 @@ def adminAction():
         showHomeMenu()
     else:
         print("Invalid action code")
+
 def adminLogin():
     print("Enter username:")
     username = input()
     encpass = base64.b64encode(maskpass.advpass().encode("utf-8"))
-    
     with open(".admin_credentials", "r") as f:
         lines = f.read().split('\n')
         cr_user = base64.b64encode(lines[0].encode("utf-8"))
@@ -161,14 +180,14 @@ def adminLogin():
         print("Invalid username or password!")
 
 def showVehicleList(listname):
-    with open(listname,"r") as vehicle_list:
+    with open(listname, "r") as vehicle_list:
         reader = csv.reader(vehicle_list)
         rows = list(vehicle_list)
     for row in range(len(rows)):
         if row == 0:
             print("   " + rows[row])
             continue
-        print(str(row) +", " + rows[row])
+        print(str(row) + ", " + rows[row])
 
 def collectRenterInfo():
     firstName = input("Enter first name: ")
@@ -179,45 +198,42 @@ def collectRenterInfo():
     email = input("Enter e-mail: ")
     phone_number = input("Enter phone number: ")
     rent_period = int(input("Enter rent period - days: "))
-    renter = RentalPerson(firstName, family, nationality, identification_number, card_id, email, phone_number, rent_period)
+    renter = RentalPerson(firstName, family, nationality,
+                          identification_number, card_id, email, phone_number, rent_period)
     return renter
 
 def bookVehicle(listname):
-    action = int(input("Enter vehicle id or 0 to go to home"))
+    action = int(input("Enter vehicle id or 0 to go to home: "))
     if action == 0:
         showHomeMenu()
     else:
         if action > 0 and action < len(listname):
-            with open(listname,"r") as vehicle_list:
-                #reader = csv.reader(vehicle_list)
-                rows = list(vehicle_list) 
+            with open(listname, "r") as vehicle_list:
+                rows = list(vehicle_list)
         vehicle_info = rows[action].split(",")
         renter = collectRenterInfo()
         if listname == "CarsList.csv":
-            car = Car(vehicle_info[0], vehicle_info[1], vehicle_info[2], vehicle_info[3], vehicle_info[4], vehicle_info[5], vehicle_info[6])
+            car = Car(vehicle_info[0], vehicle_info[1], vehicle_info[2],
+                      vehicle_info[3], vehicle_info[4], vehicle_info[5], vehicle_info[6])
             doc = Document(car, renter)
             car.reserve(date.today(), renter.rent_period)
         elif listname == "MotorbikesList.csv":
-            motorbike = MotorBike(vehicle_info[0], vehicle_info[1], vehicle_info[2], vehicle_info[3], vehicle_info[4], vehicle_info[5], vehicle_info[6])
+            motorbike = MotorBike(vehicle_info[0], vehicle_info[1], vehicle_info[2],
+                                  vehicle_info[3], vehicle_info[4], vehicle_info[5], vehicle_info[6])
             doc = Document(motorbike, renter)
             motorbike.reserve(date.today(), renter.rent_period)
         doc.generateAgreement()
-            
-            
-            
-            
-        
+        showHomeMenu()
 
 def showHomeMenu():
-    print("Welcome to Car rental software")
-    print("Statrting with user priviliges")
     print("Enter 1 to choose car")
     print("Enter 2 to choose Motorbike")
     print("Enter 3 to login as admin")
     print("Enter 0 to close")
     action = int(input("choose action: "))
     if action == 0:
-        exit()
+        print("Goodbye!")
+        sys.exit()
     elif action == 1:
         print("Loading cars list")
         cars = "CarsList.csv"
@@ -231,20 +247,7 @@ def showHomeMenu():
     elif action == 3:
         print("Logging as admin")
         adminLogin()
-    
 
-#Main
+print("Welcome to Car rental software")
+print("Statrting with user priviliges")
 showHomeMenu()
-    
-
-
-    
-'''my_car = Car(brand = "Dacia", model="Duster", reg_plate="r 1352 kn", year="2024", color="white", mileage="10000", car_type="SUV")
-my_car.reserve(datetime.time,5)
-my_bike = MotorBike(brand="KTM", model="Duke", reg_plate="r 1234 ak", year="2016", color="orange", mileage="20000", cc=390)
-my_bike.reserve(datetime.time,3)
-renter = RentalPerson(firstName="Deyan", family="Rusev",nationality="Bulgarian",identification_number="1234", card_id="12334",email="drusev@gmail.com",phone_number="0887610392")
-renter.toString()
-doc = Document("car_rental2.txt")
-doc.generateAgreement(renter.firstName, renter.family, renter.identification_number, renter.card_id, my_car.brand, my_car.model,my_car.reg_plate,my_car.year, my_car.color, my_car.getRentalFee(5))
-'''
